5cbd5964e4d1792a0b79a7e43b86bf37cd699840
hhtetwei <hhtet263510@gmail.com>
2024-01-22 16:50:29 +0630

    Adding divider

---
+++ src/pages/Bmi/bmi.jsx
@@ -0,0 +1,15 @@
+import Divider from '@mui/material/Divider'
+const BMI = () => {
+  return (
+    <>
+      <div className="w-full h-full">
+        <div className="w-full text-start text-black p-3 mr-3 text-sm">
+          BMI calculator
+        </div>
+        <Divider />
+      </div>
+    </>
+  )
+}
+
+export default BMI
