e67597435b7764193224f299929f975a9ea4c530
hhtetwei <hhtet263510@gmail.com>
2024-01-23 16:46:10 +0630

    in wip

--- src/pages/Bmi/bmi.jsx
+++ src/pages/Bmi/bmi.jsx
@@ -1,12 +1,16 @@
 import Divider from '@mui/material/Divider'
+import BmiCard from './bmiCard'
 const BMI = () => {
   return (
     <>
-      <div className="w-full h-full">
-        <div className="w-full text-start text-black p-3 mr-3 text-sm">
+      <div className="w-full h-screen overflow-auto relative bg-slate-200">
+        <div className="w-full h-[10%] text-center text-black p-3 mr-3 text-sm">
           BMI calculator
+          <div className="flex justify-center">
+            <Divider color="#FDA228" sx={{ height: 2, width: '140px' }} />
+          </div>
         </div>
-        <Divider />
+        <BmiCard />
       </div>
     </>
   )
